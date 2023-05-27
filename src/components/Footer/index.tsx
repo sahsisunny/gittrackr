d63fffc0d31fa9d9ea19799b57cb852faa535400
SahsiSunny <sahsisunny@gmail.com>
2023-05-27 10:31:34 +0530

    Add repo list section

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      Designed and developed by &nbsp;
+      <a href="#">Sunny Sahsi</a>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -4,6 +4,7 @@ import { useSession } from 'next-auth/react';
 import Navbar from '@/components/Navbar';
 import Head from 'next/head';
 import ProfileImage from '../assets/dummyProfileImage.png';
+import Footer from '@/components/Footer';
 
 
 
@@ -27,6 +28,16 @@ const ProfilePage = () => {
       console.error('Error fetching data:', error);
     }
   };
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = date.toLocaleDateString(undefined, options);
+    const formattedTime = date.toLocaleTimeString();
+
+    return `${formattedDate} ${formattedTime}`;
+  }
+
 
   useEffect(() => {
     if (session) {
@@ -52,125 +63,133 @@ const ProfilePage = () => {
       </Head>
       <Navbar />
       <div className="main-container">
-        <div className="pd-ltr-20 xs-pd-20-10">
-          <div className="min-height-200px">
-            <div className="row">
-              <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 mb-30">
-                <div className="pd-20 card-box height-100-p">
-                  <div className="profile-photo">
+        <div className="profile-section">
+          <div className="profile-photo">
+            <Image
+              src={data?.avatar_url || ProfileImage}
+              alt="User Avatar"
+              className="avatar-photo"
+              width={200}
+              height={200}
+            />
+          </div>
+          {
+            data?.name ? (
+              <h5 className="text-center h5 mb-0">
+                {data?.name || 'No name'}
+              </h5>
+            ) : (
+              <h5 className="text-center h5 mb-0">
+                {data?.login || 'No username'}
+              </h5>
+            )
+          }
+          <p className="text-center text-muted font-14">
+            {data?.login || 'No username'}
+          </p>
+          <div className="follow">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <span>Followers</span>
+                    <hr />
+                    <h5>{data?.followers}</h5>
+                  </td>
+                  <td>
+                    <span>Following</span>
+                    <hr />
+                    <h5>{data?.following}</h5>
+                  </td>
+                  <td>
+                    <span>Public</span>
+                    <hr />
+                    <h5>{data?.public_repos}</h5>
+                  </td>
+                  <td>
+                    <span>Private</span>
+                    <hr />
+                    <h5>{data?.total_private_repos}</h5>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          {/* Orgs */}
+          <div>
+            <h5 className="profile-title">Organizations</h5>
+            <div className="profile-orgs-container">
+              {orgsData?.map((org) => (
+                <div key={org.id}>
+                  <div
+                    className="profile-orgs"
+                    onClick={() => {
+                      window.open(
+                        `https://github.com/${org.login}`,
+                        '_blank'
+                      );
+                    }}
+                  >
                     <Image
-                      src={data?.avatar_url || ProfileImage}
+                      src={org.avatar_url}
                       alt="User Avatar"
-                      className="avatar-photo"
-                      width={200}
-                      height={200}
+                      className="org-photo"
+                      width={50}
+                      height={50}
                     />
-                  </div>
-                  {
-                    // render name if it exists, otherwise render username
-
-                    data?.name ? (
-                      <h5 className="text-center h5 mb-0">
-                        {data?.name || 'No name'}
-                      </h5>
-                    ) : (
-                        <h5 className="text-center h5 mb-0">
-                          {data?.login || 'No username'}
-                        </h5>
-                      )
-                  }
-                  <p className="text-center text-muted font-14">
-                    {data?.login || 'No username'}
-                  </p>
-                  <div className="follow">
-                    <table>
-                      <tbody>
-                        <tr>
-                          <td>
-                            <span>Followers</span>
-                            <hr />
-                            <h5>{data?.followers}</h5>
-                          </td>
-                          <td>
-                            <span>Following</span>
-                            <hr />
-                            <h5>{data?.following}</h5>
-                          </td>
-                          <td>
-                            <span>Public</span>
-                            <hr />
-                            <h5>{data?.public_repos}</h5>
-                          </td>
-                          <td>
-                            <span>Private</span>
-                            <hr />
-                            <h5>{data?.total_private_repos}</h5>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                  <div>
-                    <h5 className="profile-title">Organizations</h5>
-                    <div className="profile-orgs-container">
-                      {orgsData?.map((org) => (
-                        <div key={org.id}>
-                          <div
-                            className="profile-orgs"
-                            onClick={() => {
-                              window.open(
-                                `https://github.com/${org.login}`,
-                                '_blank'
-                              );
-                            }}
-                          >
-                            <Image
-                              src={org.avatar_url}
-                              alt="User Avatar"
-                              className="org-photo"
-                              width={50}
-                              height={50}
-                            />
-                            <span>{org.login}</span>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                  <div>
-                    <h5 className="profile-title">Repositories</h5>
-                    <button
-                      className="accordion"
-                      onClick={() => {
-                        setShowDropdown(!showDropdown);
-                      }}
-                    >
-                      See Repositories
-                    </button>
-                    <div
-                      className="panel"
-                      style={{ display: showDropdown ? 'block' : 'none' }}
-                    >
-                      {reposData?.map((repo) => (
-                        <div key={repo.id}>
-                          <div
-                            className="repos"
-                            onClick={() => {
-                              window.open(`${repo.html_url}`, '_blank');
-                            }}
-                          >
-                            <span>{repo.name}</span>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
+                    <span>{org.login}</span>
                   </div>
                 </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
+        <div className="repos-section">
+          <h5 className="profile-title">Repositories</h5>
+          <div
+            className="repo-list"
+          >
+            {reposData?.map((repo) => (
+              <div key={repo.id}>
+                <div
+                  className="repo-item"
+                  onClick={() => {
+                    window.open(`${repo.html_url}`, '_blank');
+                  }}
+                ><div className="repo-details">
+                    
+                  <div className="repo-item-left">
+                    <span
+                      className="repo-item-name"
+                    >{repo.name}</span>
+                </div>
+                  <div className="repo-item-right">
+                    <span
+                      className="repo-item-language"
+                    >{repo.language}</span>
+                    <span
+                      className="repo-item-privacy"
+                    >{repo.private ? 'Private' : 'Public'}</span>
+                    <span
+                      className="repo-item-updated"
+                    >{formatDate(repo.updated_at)}</span>
+                    </div>
+                  </div>
+                    <button
+                      className="repo-view-btn"
+                      onClick={() => {
+                        window.open(`${repo.html_url}`, '_blank');
+                      }}
+                    >
+                      Open
+                    </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
   );
 };
