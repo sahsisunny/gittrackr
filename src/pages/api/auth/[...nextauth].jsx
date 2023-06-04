0244bd19fc4c7300f008869b75f6cf4e6563baad
SahsiSunny <sahsisunny@gmail.com>
2023-06-04 06:36:49 +0530

    test

--- src/pages/api/auth/[...nextauth].jsx
+++ src/pages/api/auth/[...nextauth].jsx
@@ -19,22 +19,22 @@ export default NextAuth({
   ],
   callbacks: {
     async session({ session, token }) {
-      const userResponse = await fetch('https://api.github.com/user', {
-        headers: {
-          Authorization: `token ${token.accessToken}`,
-        },
-      });
-      const user = await userResponse.json();
+      // const userResponse = await fetch('https://api.github.com/user', {
+      //   headers: {
+      //     Authorization: `token ${token.accessToken}`,
+      //   },
+      // });
+      // const user = await userResponse.json();
       session.user.id = token.id;
       session.accessToken = token.accessToken;
-      session.user.login = user.login;
-      session.user.url = user.url;
-      session.user.repos_url = user.repos_url;
-      session.user.followers = user.followers;
-      session.user.following = user.following;
-      session.user.public_repos = user.public_repos;
-      session.user.organizations_url = user.organizations_url;
-      session.user.created_at = user.created_at;
+      // session.user.login = user.login;
+      // session.user.url = user.url;
+      // session.user.repos_url = user.repos_url;
+      // session.user.followers = user.followers;
+      // session.user.following = user.following;
+      // session.user.public_repos = user.public_repos;
+      // session.user.organizations_url = user.organizations_url;
+      // session.user.created_at = user.created_at;
       return session;
     },
     async jwt({ token, user, account }) {
