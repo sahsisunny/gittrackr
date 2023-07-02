06e1fd888e8eb038861fbc20697ef49181e2ee8a
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-07-03 02:52:18 +0530

    fixed the mock file

--- src/constants/helpQnQ.mock.ts
+++ src/constants/helpQnQ.mock.ts
@@ -6,23 +6,23 @@ export const helpQnAMock = [
       'Open a web browser and navigate to the GitHub website at' +
       '\nLog in to your GitHub account using your credentials.\n' +
       '\nIn the top-right corner of the GitHub page, click on your profile picture or avatar to open the account menu.\n' +
-      '\n  From the account menu, select "Your organizations". This will display a list of organizations you are a member of.\n' +
-      '\nLocate the organization for which you want to make your member profile public and click on its name to access the organization&apos;s page.\n' +
-      '\nOn the organization&apos;s page, navigate to the &quot;Members&quot; tab or section. This section usually lists all the members of the organization.\n' +
-      '\n Find your username or account in the list of members and click on it to access your member profile within the organization.\n' +
+      '\nFrom the account menu, select "Your organizations". This will display a list of organizations you are a member of.\n' +
+      '\nLocate the organization for which you want to make your member profile public and click on its name to access the organizations page.\n' +
+      '\nOn the organization&apos;s page, navigate to the Members tab or section. This section usually lists all the members of the organization.\n' +
+      '\nFind your username or account in the list of members and click on it to access your member profile within the organization.\n' +
       '\n Once you are on your member profile page, you need to make some changes to your organization visibility settings.\n' +
-      '\nNear the top-right corner of the member profile page, click on the &quot;Settings&quot; button. This will open the settings page for your member profile.\n' +
-      '\n  On the settings page, scroll down to the &quot;Profile visibility&quot; section.\n' +
-      '\nBy default, the profile visibility is set to &quot;Private.&quot; Click on the drop-down menu and select &quot;Public&quot; to make your member profile visible to the public.\n' +
-      '\nAfter selecting &quot;Public,&quot; the settings page may automatically save the changes. If not, look for a &quot;Save&quot; or &quot;Update&quot; button on the page and click it to save the changes.\n' +
-      '\nOnce you have saved the changes, your member profile within the GitHub organization should now be public and visible to anyone who visits the organization&apos;s page.\n',
+      '\nNear the top-right corner of the member profile page, click on the Settings button. This will open the settings page for your member profile.\n' +
+      '\n  On the settings page, scroll down to the Profile visibility section.\n' +
+      '\nBy default, the profile visibility is set to Private. Click on the drop-down menu and select Public to make your member profile visible to the public.\n' +
+      '\nAfter selecting Public, the settings page may automatically save the changes. If not, look for a Save or Update button on the page and click it to save the changes.\n' +
+      '\nOnce you have saved the changes, your member profile within the GitHub organization should now be public and visible to anyone who visits the organizations page.\n',
   },
   {
     id: 2,
     question: 'What is GitTrackr?',
     answer:
       'Open a web browser and navigate to the GitHub website at' +
-      '\n  GitTrackr is a web application that allows you to track the issue and pull request activity of your GitHub organization\n' +
+      '\nGitTrackr is a web application that allows you to track the issue and pull request activity of your GitHub organization\n' +
       '\nGitTrackr provides a dashboard that displays the number of\n' +
       '\nissues and pull requests that have been opened and closed over\n' +
       '\na specified time period. GitTrackr also provides a list of\n' +
