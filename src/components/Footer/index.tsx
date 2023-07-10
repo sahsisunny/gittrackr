48aaa5905a1b2f5df3aa1d8f2f4f933a215b64ee
Sunny Sahsi <sahsisunny@gmail.com>
2023-07-10 09:11:51 +0530

    FIX: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,21 +7,32 @@ const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.linksContainer}>
-        <Link href="/faq">FAQ</Link>
-        <Link href="/contact-us">Contact us</Link>
-        <Link href="/">Home</Link>
+        <p>
+          Created by{' '}
+          <a href="https://discord.gg/EQAaFQkQwS" className={styles.anchorLink}>
+            Code Hunter
+          </a>
+        </p>
       </div>
-      <div>
-        <p>created by code hunter</p>
-      </div>
-      <div className={styles.githubRepoLinkContainer}>
-        Source code available on &nbsp;
+      <div className={styles.linksContainer}>
+        Source code available on
         <a
-          href="https://github.com/sahsisunny/git-dashboard"
-          className={styles.githubRepoLink}
+          href="https://github.com/sahsisunny/gittrackr"
+          className={styles.anchorLink}
         >
           GitHub
         </a>
+      </div>
+      <div className={styles.linksContainer}>
+        <Link className={styles.anchorLink} href="/faq">
+          FAQ
+        </Link>
+        <Link className={styles.anchorLink} href="/contact-us">
+          Contact us
+        </Link>
+        <Link className={styles.anchorLink} href="/">
+          Home
+        </Link>
       </div>
     </footer>
   );
