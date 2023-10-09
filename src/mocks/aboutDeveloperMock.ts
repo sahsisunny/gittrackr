41a9e11a1e39e52152477fc7210fc3e2593a5f4b
Pratiyush Kumar <82165483+Pratiyushkumar@users.noreply.github.com>
2023-10-10 01:04:37 +0530

    ABout developer feature completed (#40)

---
+++ src/mocks/aboutDeveloperMock.ts
@@ -0,0 +1,29 @@
+import developerPratiyush from '@/assets/developersImage/pratiyushKumar.jpg';
+import developerSunny from '@/assets/developersImage/sunny.jpg';
+
+export const aboutDevelopersMock = [
+  {
+    id: 1,
+    developerName: 'Sunny Sahsi',
+    developerImage: developerSunny,
+    developerIntro: `I'm a software engineer who is passionate about building websites. 
+    I am working at Infosys as FrontEnd Developer and also an open source contributor`,
+    developerTwitterId: 'https://twitter.com/SahsiSunny',
+    developerLinkedInId: 'https://www.linkedin.com/in/sahsisunny/',
+    developerGithubId: 'https://github.com/sahsisunny',
+    developerPortfolio: 'https://sahsisunny.netlify.app/',
+    side: 'left',
+  },
+  {
+    id: 2,
+    developerName: 'Pratiyush Kumar',
+    developerImage: developerPratiyush,
+    developerIntro: `I'm a software engineer who is passionate about building websites. 
+    I am working at Infosys as FrontEnd Developer and also an open source contributor`,
+    developerTwitterId: 'https://twitter.com/mein_pk',
+    developerLinkedInId: 'https://www.linkedin.com/in/pratiyushkumar/',
+    developerGithubId: 'https://github.com/Pratiyushkumar',
+    developerPortfolio: 'https://www.crio.do/learn/portfolio/pratiyush-inv/',
+    side: 'right',
+  },
+];
