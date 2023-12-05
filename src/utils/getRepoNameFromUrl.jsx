5ecda4470db189b132b92779df5c36a08386799d
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-05 14:35:40 +0530

    removed all files

--- src/utils/getRepoNameFromUrl.jsx
+++
@@ -1,9 +0,0 @@
-const getRepoNameFromUrl = (url) => {
-  const parseUrl = new URL(url);
-  const pathname = parseUrl.pathname;
-  const parts = pathname.split('/');
-  const repoName = parts[parts.length - 1];
-  return repoName;
-};
-
-export default getRepoNameFromUrl;
